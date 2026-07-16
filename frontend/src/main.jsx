589605f2de304dca0ae4bdb9b7f84e861235034e
aymen-khelifa <gytgutu@gmail.com>
2026-07-16 02:54:45 +0200

    fix: rename App.jsx to AppEntry.jsx to fully bypass Vercel's broken cache for this file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './AppEntry.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
